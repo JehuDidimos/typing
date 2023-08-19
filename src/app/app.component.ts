2b02f4e585a1b1a41bd35bdc1b77b523a719b331
JehuDidimos <jehudidimos.cs@gmail.com>
2023-08-19 12:35:37 -0500

    Text comparison and text status indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem, random } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typing';
+
+  protected randomSentence: string;
+  protected inputSentence: string;
+
+  constructor(){
+    this.randomSentence = lorem.sentence();
+    this.inputSentence = '';
+  }
+
+  retrieveInput(sentence: any){
+    this.inputSentence = sentence.value;
+  }
+
+  compare(randomLetter: string, inputLetter: string){
+    if(!inputLetter){
+      return 'pending'
+    }
+
+    return randomLetter === inputLetter ? 'correct':'incorrect';
+  }
 }
